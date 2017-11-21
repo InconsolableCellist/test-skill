b8310f3f1c18994e3f48bc293ab6e779e04eca28
InconsolableCellist <na>
2017-11-20 19:34:40 -0500

    initial check-in

---
+++ __init__.py
@@ -0,0 +1,39 @@
+import sys
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from mycroft.messagebus.message import Message
+from os.path import dirname, abspath, basename
+from pprint import pprint, pformat
+
+sys.path.append(abspath(dirname(__file__)))
+
+__author__ = 'InconsolableCellist'
+
+LOGGER = getLogger(__name__)
+
+class TestSkill(MycroftSkill): 
+    def __init__(self): 
+        super(TestSkill, self).__init__(name="TestSkill")
+
+    def initialize(self):
+        LOGGER.info("initializing TestSkill")
+
+        test_intent = IntentBuilder("TestIntent"). \
+                require("TestKeyword"). \
+                require("Artist"). \
+                build()
+        self.register_intent(test_intent, self.handle_test_intent)
+
+    def stop(self):
+        pass
+
+    def handle_test_intent(self, message):
+        LOGGER.info("message.data is {}".format(pformat(message.data)))
+
+        if 'Artist' in message.data:
+            LOGGER.info("Artist found! {}".format(message.data['Artist']))
+
+def create_skill(): 
+    return TestSkill()
+
